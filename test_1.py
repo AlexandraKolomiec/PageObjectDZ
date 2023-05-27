88fb7350bdefa410c1964f0ad6787a3039925df5
KM Alexandra <Sasha-Km@yandex.ru>
2023-05-27 21:07:30 +0300

    DZ3

---
+++ test_1.py
@@ -0,0 +1,22 @@
+from testpage2 import OperationsHelper
+import logging
+"""Добавить в проект тест по проверке механики работы формы Contact Us на главной странице личного кабинета.
+ Должно проверятся открытие формы, ввод данных в поля, клик по кнопке и появление всплывающего alert.
+Совет: переключиться на alert можно командой alert = self.driver.switch_to.alert  Вывести текст alert.text """
+
+
+def test_step1(browser):
+    
+    logging.info('Test1 Starting')
+    testpage = OperationsHelper(browser)
+    testpage.go_to_site()
+    testpage.enter_login('NatGlu')
+    testpage.enter_pass('db2d28bf9f')
+    testpage.click_login_button()
+    #assert testpage.get_error_text() == "401"
+    testpage.click_contact_button()
+    testpage.enter_name('Наталья Глушко')
+    testpage.enter_email('NatGlu@yandex.ru')
+    testpage.enter_content('HELLO')
+    testpage.click_contact_us_button()
+    assert testpage.alert() == 'Form successfully submitted'
